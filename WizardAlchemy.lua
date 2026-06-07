c0c055eee68500439b9e52ce803ebf07b8bc0de2
Kyle VanSaun <kavansaun@ku.edu>
2026-06-07 00:24:52 -0500

    Update WizardAlchemy.lua

--- WizardAlchemy.lua
+++ WizardAlchemy.lua
@@ -1,2 +1,2 @@
-script_key="jIFckNrKogPPLDFyGTeHmZWuhKGtDUYs";
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/673fccd83442f4d820f57c439b4bc4cd.lua"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/user2084375/ef0c1434dc71b6bec4349ee57b04f1b5/raw/66f8d272ceecb1117226b0c7fd72a5850403399f/gistfile1.txt"))()
+
