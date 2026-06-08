00f7a903db291b4dc59e99c83b93a13bbd9db77c
Kyle VanSaun <kavansaun@ku.edu>
2026-06-07 19:18:52 -0500

    Update TEF.lua

--- TEF.lua
+++ TEF.lua
@@ -2,7 +2,7 @@ local Players = game:GetService("Players")
 local TweenService = game:GetService("TweenService")
 local RunService = game:GetService("RunService")
 local CoreGui = game:GetService("CoreGui")
-
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local LocalPlayer = Players.LocalPlayer
 
 -- ==========================================
